b2203d980c9a06cc4b42d9a4a71ee82bd459d060
Oniby <evargandolfi98@gmail.com>
2025-04-09 22:12:59 +0200

    Day 2 completed

--- Solutions/Day-2.py
+++ Solutions/Day-2.py
@@ -1,23 +1,52 @@
 import re
 from utils import *
 
-def is_safe(report_str:str) -> bool:
-    pattern = re.compile(r'\d+')
-    m = re.findall(pattern, report_str)
-    report = [int(l) for l in m]
-    for i in range(1,len(report)):
-        diff = report[i] - report[i-1]
-        if diff*(report[1]-report[0])<0:
+def is_safe(report: list[int]):
+    for i in range(1, len(report)):
+        diff = report[i] - report[i - 1]
+        if diff * (report[1] - report[0]) < 0:
             return False
-        elif abs(diff)<1 or abs(diff)>3:
+        elif abs(diff) < 1 or abs(diff) > 3:
             return False
     return True
 
-puzzle_input = read_full_input(2, False)
+def report_from_string(report_string: str):
+    pattern = re.compile(r'\d+')
+    m = re.findall(pattern, report_string)
+    report = [int(l) for l in m]
+    return report
 
-n_safe = 0
-for report in puzzle_input.split('\n'):
-    if is_safe(report):
-        n_safe += 1
+if __name__ == "__main__":
 
-print(f"There are {n_safe} safe reports.")
+    part = 'B'
+
+    if part == 'A':
+        ### PART A
+        puzzle_input = read_full_input(2, False)
+
+        n_safe = 0
+        for report_str in puzzle_input.split('\n'):
+            report = report_from_string(report_str)
+            if is_safe(report):
+                n_safe += 1
+
+        print(f"There are {n_safe} safe reports.")
+
+    ### PART B
+    if part == 'B':
+
+        puzzle_input = read_full_input(2, False)
+
+        n_safe = 0
+        for report_str in puzzle_input.split('\n'):
+            report = report_from_string(report_str)
+            if is_safe(report):
+                n_safe += 1
+                continue
+            for i in range(len(report)):
+                cut_report = report[:i] + report[i + 1:]
+                if is_safe(cut_report):
+                    n_safe += 1
+                    break
+
+        print(f"There are {n_safe} safe reports.")
