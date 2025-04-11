9c20b0e859230f6cc0a47d01629645dabf21fc7e
Oniby <evargandolfi98@gmail.com>
2025-04-12 01:14:07 +0200

    Day 3 part B done

--- Solutions/Day-3.py
+++ Solutions/Day-3.py
@@ -17,9 +17,34 @@ def sum_products(products):
         sum += product
     return sum
 
+def extract_tokens(string):
+    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)")
+    items = pattern.finditer(string)
+    tokens = []
+    for m in items:
+        if m.lastindex:
+            x = m.group(1)
+            y = m.group(2)
+            tokens.append((int(x), int(y)))
+        else:
+            tokens.append(m.group(0))
+    return tokens
+
+def process_tokens(tokens):
+    do = True
+    sum = 0
+    for t in tokens:
+        if t == "do()":
+            do = True
+        elif t == "don't()":
+            do = False
+        else:
+            sum += (t[0]*t[1])*(1 if do else 0)
+    return sum
+
 if __name__ == '__main__':
 
-    part = 'A'
+    part = 'B'
 
     match part:
         case 'A':
@@ -30,4 +55,11 @@ if __name__ == '__main__':
             products = multiply_tuples(tuples)
             sum = sum_products(products)
 
+            print(f"Final sum: {sum}")
+
+        case 'B':
+
+            input = read_full_input(3,False)
+            tokens = extract_tokens(input)
+            sum = process_tokens(tokens)
             print(f"Final sum: {sum}")
